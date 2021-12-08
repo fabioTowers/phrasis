772c3f719460f3a6374f71c20e99f80d53b3fc80
Fabio Mendes <fabiomendes53@gmail.com>
2021-12-08 12:15:54 -0300

    feat: create phrase controller

---
+++ app/Http/Controllers/PhrasesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PhrasesController extends Controller
+{
+    public function index() {
+        $phrases = Phrase::all();
+        return view('index', ['phrases' => $phrases]);
+    }
+
+    public function edit( Phrase $phrase ) {
+        return view('phrases.edit', ['phrase' => $phrase]);
+    }
+
+    public function update( Phrase $phrase ) {
+        request()->validate([
+            'content' => 'required',
+            'author' => 'required',
+        ]);
+        $phrase->update([
+            'content' => request('content'),
+            'author' => request('author'),
+        ]);
+        return redirect('/phrases');
+    }
+
+    public function create() {
+        return view('phrases.create');
+    }
+
+    public function store() {
+        request()->validate([
+            'content' => 'required',
+            'author' => 'required',
+        ]);
+        Phrase::create([
+            'content' => request('content'),
+            'author' => request('author'),
+        ]);
+        return redirect('/phrases');
+    }
+
+    public function destroy(Phrase $phrase) {
+        $phrase->delete();
+        return redirect('/phrases');
+    }
+}
